dc4d8ec7a53064385dd01381b1baf2e83131e7fd
Emilia Hansson <eh222yn@student.lnu.se>
2023-02-20 22:37:55 +0100

    Add text to when there's no search results

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -8,11 +8,16 @@ function SearchResults () {
 
   return ( 
     <>
-      <div id='searchResults'>
+      { searchResults.length !== 0
+      ? <div className='searchResults'>
         {searchResults?.map((movie) => {
           return <MovieCardVertical id={movie.id} title={movie.title} year={getYear(new Date(movie.release_date))} poster={movie.poster_path} />
         })}
       </div>
+      : <div className='searchResults'>
+          <p id='noResultsText'>Oops, couldn't find a movie with the given title...</p>
+        </div>
+      }
     </> 
   )
 }
