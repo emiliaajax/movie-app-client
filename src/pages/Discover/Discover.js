55cfd925a7fca5043538d64d5597c9a1cc23ed26
Emilia Hansson <eh222yn@student.lnu.se>
2023-02-20 21:35:07 +0100

    Edit design of discover page

--- src/pages/Discover/Discover.js
+++ src/pages/Discover/Discover.js
@@ -13,11 +13,16 @@ function Discover () {
       <div className='discoverWrapper'>
         <div className='discoverContainer'>
           <h1>Discover</h1>
-          <GenreMenu />
+          <div id='genreMenu'>
+            <GenreMenu />
+          </div>
           <div id="discoverResults">
-            {moviesByGenre?.map((movie) => {
-              return <MovieCardVertical id={movie.id} title={movie.title} year={getYear(new Date(movie.release_date))} poster={movie.poster_path} />
-            })}
+            {moviesByGenre
+            ? moviesByGenre.map((movie) => {
+                return <MovieCardVertical id={movie.id} title={movie.title} year={getYear(new Date(movie.release_date))} poster={movie.poster_path} />
+              })
+            : <p id='chooseGenre'>Choose a genre...</p>
+            }
           </div>
         </div>
         <div className='footer'>
